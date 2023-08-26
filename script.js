a97ae3af1b752ac7afd7d90b678f521e81a4ec2c
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-08-26 19:11:15 +0600

    text bold, italic  and underline handler updated

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("connected");
+const field = document.getElementById("field");
+
+const bold = document.getElementById("bold");
+bold.addEventListener("click", function () {
+  field.classList.toggle("font-bold");
+  bold.classList.toggle("text-red-400");
+});
+
+const italic = document.getElementById("italic");
+italic.addEventListener("click", function () {
+  field.classList.toggle("italic");
+  italic.classList.toggle("text-red-400");
+});
+
+const underline = document.getElementById("underline");
+underline.addEventListener("click", function () {
+  field.classList.toggle("underline");
+  underline.classList.toggle("text-red-400");
+});
